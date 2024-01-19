7c6e99d98106335cd17f8d340a19b1862d2113d8
Joy Brar <joybrar2001@gmail.com>
2024-01-19 21:22:21 +0530

    Added graphs

--- src/screens/AppDetailsScreen.js
+++ src/screens/AppDetailsScreen.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
-import { Entypo, FontAwesome } from '@expo/vector-icons';
+import { Entypo, FontAwesome, Feather } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { appDetails } from '../constants/data';
 import { useNavigation } from '@react-navigation/native';
+import { LineChart } from 'react-native-chart-kit';
 
 const AppDetailsScreen = ({ route, navigation }) => {
   const app = route.params;
@@ -23,6 +24,9 @@ const AppDetailsScreen = ({ route, navigation }) => {
       </View>
 
       <View className="mt-8">
+        <TouchableOpacity onPress={() => navigation.goBack()} className="z-10 absolute left-2 bg-white rounded-full">
+          <Entypo name="chevron-left" size={40} color="black" />
+        </TouchableOpacity>
         <Text className="text-white text-3xl font-semibold text-center">{app.name}</Text>
         <View className="flex justify-center items-center mx-auto mt-4">
           <Image source={app.imageSrc} style={{ height: 120, width: 120, resizeMode: 'contain' }} />
@@ -30,7 +34,22 @@ const AppDetailsScreen = ({ route, navigation }) => {
       </View>
 
       <View className="px-6 mt-4">
-        <View className="flex flex-row gap-4 justify-center items-center">
+        <View className="flex flex-row gap-4 justify-center items-center mb-4">
+          <View className="flex flex-1 justify-center items-center bg-white/20 py-2 rounded-lg">
+            <Feather name="arrow-up-right" size={24} color="white" />
+            <Text className="text-white text-lg">Open</Text>
+          </View>
+          <View className="flex flex-1 justify-center items-center bg-white/20 py-2 rounded-lg">
+            <Feather name="stop-circle" size={24} color="white" />
+            <Text className="text-white text-lg">Force Stop</Text>
+          </View>
+          <View className="flex flex-1 justify-center items-center bg-white/20 py-2 rounded-lg">
+            <Feather name="trash" size={24} color="white" />
+            <Text className="text-white text-lg">Uninstall</Text>
+          </View>
+        </View>
+
+        <View className="flex flex-row gap-4 justify-center items-center mb-4">
           <View className="flex flex-1 justify-center items-center bg-white/20 py-2 rounded-lg">
             <Text className="text-white text-sm font-medium text-white/90">Storage</Text>
             <Text className="text-white text-lg"><Text className="font-bold">{app.storage}</Text> MB</Text>
@@ -39,6 +58,52 @@ const AppDetailsScreen = ({ route, navigation }) => {
             <Text className="text-white text-sm font-medium text-white/90">Time used</Text>
             <Text className="text-white text-lg"><Text className="font-bold">{app.usage}</Text> hrs</Text>
           </View>
+        </View>
+
+        <View className="mx-auto">
+          <LineChart
+            data={{
+              labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+              datasets: [
+                {
+                  data: [
+                    Math.random() * 24,
+                    Math.random() * 24,
+                    Math.random() * 24,
+                    Math.random() * 24,
+                    Math.random() * 24,
+                    Math.random() * 24,
+                    Math.random() * 24,
+                  ]
+                }
+              ]
+            }}
+            width={Dimensions.get("window").width*0.9}
+            height={220}
+            yAxisSuffix=" hrs"
+            yAxisInterval={1}
+            chartConfig={{
+              backgroundColor: "#4B8E4B",
+              backgroundGradientFrom: "#4B8E4B",
+              backgroundGradientTo: "#4B8E4B",
+              decimalPlaces: 0,
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 10
+              },
+              propsForDots: {
+                r: "6",
+                strokeWidth: "2",
+                stroke: "#4B8E4B"
+              }
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+          />
         </View>
       </View>
     </SafeAreaView>
